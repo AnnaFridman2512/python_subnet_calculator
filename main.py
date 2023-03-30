bfb0e5f0edfa8224afee5253f5f25ced7fab0a5b
anna <anna@ubuntu.myguest.virtualbox.org>
2023-03-30 07:37:17 +0300

    calc_network_class func added, calc_CIDR func modified

--- main.py
+++ main.py
@@ -8,26 +8,42 @@ def check_ip(ip_str):  # Will return True or False  (verify – hint: regex)
 
 #check_ip("192.168.0.1")
 
-def check_CIDR(cidr):  # Cheks if CIDR valid for given IP address
-    if not cidr.isdigit() or not 0 <= cidr <= 32:
-        print("Enter only numbers between 0 and 32")
-    else:
-        cidr = int(cidr)
-
-
-def calc_CIDR(ip_str):  # Calculates the CIDR by class table and return the CIDR
+def calc_network_class(ip_str):
     ip_list = ip_str.split('.')
     first_octate = int(ip_list[0])
-    print(first_octate)
     if first_octate <= 126:
-        cidr = 8
+        network_class = "A"
     elif first_octate <= 191:
-        cidr = 16
+        network_class = "B"
     elif first_octate <= 223:
+        network_class= "C"
+    else:
+        print("CIDR error")
+    return network_class
+
+def calc_CIDR(ip_str):  # Calculates the CIDR by class table and return the CIDR
+    network_class = calc_network_class(ip_str)
+    if network_class == "A":
+        cidr = 8
+    elif network_class == "B":
+        cidr = 16
+    elif network_class == "C":
         cidr = 24
     else:
         print("CIDR error")
     return cidr
+}
+def check_CIDR_match_network(cidr):  #Cheks if CIDR valid for given IP address
+    if not cidr.isdigit() or not 0 <= cidr <= 32:
+        print("Enter only numbers between 0 and 32")
+    else:
+        cidr = int(cidr)
+        #If the network is class A CIDR should be from /8 to /1 and from /25 to /31
+        #If the network is class B CIDR should be from /16 to /9 and from /25 to /31
+        #If the network is class C CIDR should be from /31 to /17
+
+
+
 
 
 # calc_CIDR("192.168.0.1")
@@ -43,7 +59,7 @@ def sub_calc():
             user_CIDR = input("Enter CIDR")
             if user_CIDR:
                 # check if CIDR is right for the IP
-                check_CIDR()
+                check_CIDR_match_network
             else:
                 # calculate the CIDR by IP class
                 calc_CIDR(ip_address)
