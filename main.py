f287602838acabd02a24906016fb33bac874cf4a
anna <anna@ubuntu.myguest.virtualbox.org>
2023-03-30 08:15:02 +0300

    calc_network_class,calc_CIDR,check_CIDR_match_network functions modified

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def check_ip(ip_str):  # Will return True or False  (verify – hint: regex)
         re.match(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', ip_str))
 
 
-#check_ip("192.168.0.1")
+# check_ip("192.168.0.1")
 
 def calc_network_class(ip_str):
     ip_list = ip_str.split('.')
@@ -16,37 +16,62 @@ def calc_network_class(ip_str):
     elif first_octate <= 191:
         network_class = "B"
     elif first_octate <= 223:
-        network_class= "C"
+        network_class = "C"
     else:
-        print("CIDR error")
+        print("Not valid IP address")
+    # print(network_class)
     return network_class
 
+
+# calc_network_class("224.0.0.1")
 def calc_CIDR(ip_str):  # Calculates the CIDR by class table and return the CIDR
     network_class = calc_network_class(ip_str)
     if network_class == "A":
-        cidr = 8
+        calculated_CIDR = 8
     elif network_class == "B":
-        cidr = 16
+        calculated_CIDR = 16
     elif network_class == "C":
-        cidr = 24
+        calculated_CIDR = 24
     else:
-        print("CIDR error")
-    return cidr
-}
-def check_CIDR_match_network(cidr):  #Cheks if CIDR valid for given IP address
-    if not cidr.isdigit() or not 0 <= cidr <= 32:
-        print("Enter only numbers between 0 and 32")
-    else:
-        cidr = int(cidr)
-        #If the network is class A CIDR should be from /8 to /1 and from /25 to /31
-        #If the network is class B CIDR should be from /16 to /9 and from /25 to /31
-        #If the network is class C CIDR should be from /31 to /17
-
-
-
-
+        print("IP is not valid")
+    return calculated_CIDR
 
 # calc_CIDR("192.168.0.1")
+
+
+def check_CIDR_match_network(user_CIDR, ip_address):  # Cheks if CIDR valid for given IP address
+    if not 1 <= user_CIDR <= 32:
+        print("Enter only numbers between 1 and 32")
+    else:
+        user_CIDR = int(user_CIDR)
+        network_class = calc_network_class(ip_address)
+        print(network_class)
+        if 25 <= user_CIDR <= 31:
+            #print("True")
+            return True
+
+        # If the network is class A CIDR should be from /8 to /1
+        elif user_CIDR <= 8 and network_class == "A":
+            #print("True")
+            return True
+
+        # If the network is class B CIDR should be from /16 to /9
+        elif 9 <= user_CIDR <= 16 and network_class == "B":
+            #print("True")
+            return True
+        # If the network is class C CIDR should be from /31 to /17
+        elif 17 <= user_CIDR <= 31 and network_class == "C":
+            #print("True")
+            return True
+        else:
+            #print("False")
+            return False
+
+
+#check_CIDR_match_network(16, "192.168.0.1")
+
+
+
 
 
 """def calc_by_host()#Should verify that input valid also
